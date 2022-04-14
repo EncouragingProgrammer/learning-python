164095880324f1048a281127bdba89c0c736f48a
Ryan Gustafson <jgustafson1@athabasca.edu>
2022-04-14 14:50:13 -0600

    Experimenting with working.py

--- working.py
+++ working.py
@@ -2,3 +2,26 @@
 
 print('Welcome!')
 
+# in python there is no difference between single quotes and double quotes
+# triple quotes can be used to create multi line strings
+# strings are objects so we can call methods on them
+
+# f strings
+# a = 8
+# b = 9
+# x = f'seven is {a} {b}'
+
+# {a:<09} this will left align with total size 9 and fill rest with zeros
+
+x = 'seven "{1:<09}" "{0:>09}"'.format(8, 9)
+print('x is {}'.format(x))
+print(type(x))
+
+# bitwise operators & , | , ^ , << , >>
+
+x = 0x0a
+y = 0x01
+z = x >> y
+
+print(f'(hex) x is {x:02x}, y is {y:02x}, z is {z:02x}')
+print(f'(bin) x is {x:08b}, y is {y:08b}, z is {z:08b}')
